479130162cbc57c4ed69435f6b104e3d31582acf
Pedro Tiago Monteiro <ptgm@igc.gulbenkian.pt>
2014-11-14 09:50:45 +0000

    clean up

--- src/main/java/org/ginsim/epilog/gui/widgets/ModelChooserWidget.java
+++ src/main/java/org/ginsim/epilog/gui/widgets/ModelChooserWidget.java
@@ -1,11 +1,11 @@
 package org.ginsim.epilog.gui.widgets;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 
 public class ModelChooserWidget extends JPanel {
+	private static final long serialVersionUID = -1199282868603572058L;
 
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
