ce1e98b85af12989db3d12aa955f690fc71659b5
Pedro Tiago Monteiro <ptgm@igc.gulbenkian.pt>
2015-01-20 16:52:38 +0000

    Fixed GridInformation scrollpane view

--- src/main/java/org/ginsim/epilog/gui/widgets/GridInformation.java
+++ src/main/java/org/ginsim/epilog/gui/widgets/GridInformation.java
@@ -1,5 +1,7 @@
 package org.ginsim.epilog.gui.widgets;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.util.ArrayList;
@@ -10,13 +12,14 @@ import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import org.colomoto.logicalmodel.LogicalModel;
 import org.colomoto.logicalmodel.perturbation.AbstractPerturbation;
-import org.ginsim.epilog.project.ProjectModelFeatures;
 import org.ginsim.epilog.core.EpitheliumComponentFeatures;
 import org.ginsim.epilog.core.EpitheliumGrid;
 import org.ginsim.epilog.core.EpitheliumIntegrationFunctions;
+import org.ginsim.epilog.project.ProjectModelFeatures;
 
 public class GridInformation extends JPanel {
 	private static final long serialVersionUID = -1449994132920814592L;
@@ -24,6 +27,7 @@ public class GridInformation extends JPanel {
 	private EpitheliumComponentFeatures compFeatures;
 	private EpitheliumIntegrationFunctions integrFunctions;
 	private ProjectModelFeatures modelFeatures;
+	private JPanel jIntPanel;
 
 	public GridInformation(EpitheliumComponentFeatures compFeatures,
 			EpitheliumIntegrationFunctions eif,
@@ -31,8 +35,22 @@ public class GridInformation extends JPanel {
 		this.compFeatures = compFeatures;
 		this.integrFunctions = eif;
 		this.modelFeatures = modelFeatures;
-		this.setLayout(new GridBagLayout());
+
 		this.setBorder(BorderFactory.createTitledBorder("Grid information"));
+		this.setLayout(new BorderLayout());
+
+		// JPanel
+		this.jIntPanel = new JPanel(new GridBagLayout());
+
+		// JScroll
+		JScrollPane jscroll = new JScrollPane(this.jIntPanel,
+				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		jscroll.setBorder(BorderFactory.createEmptyBorder());
+		jscroll.setMinimumSize(new Dimension(135, 0));
+		jscroll.setPreferredSize(new Dimension(135, 0));
+		this.add(jscroll, BorderLayout.CENTER);
+
 		this.updateValues(0, 0, null);
 	}
 
@@ -41,12 +59,12 @@ public class GridInformation extends JPanel {
 		gbc.gridx = 0;
 		gbc.gridwidth = 2;
 		JLabel jlTmp = new JLabel("                              ");
-		this.add(jlTmp, gbc);
+		this.jIntPanel.add(jlTmp, gbc);
 	}
 
 	public void updateValues(int posX, int posY, EpitheliumGrid grid) {
 		// TODO: LogicalModel m, AbstractPerturbation ap, byte[] state) {
-		this.removeAll();
+		this.jIntPanel.removeAll();
 		JLabel jlTmp;
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -70,7 +88,7 @@ public class GridInformation extends JPanel {
 			gbc.gridwidth = 2;
 			gbc.anchor = GridBagConstraints.WEST;
 			jlTmp = new JLabel("Cell: " + (posX + 1) + "," + (posY + 1));
-			this.add(jlTmp, gbc);
+			this.jIntPanel.add(jlTmp, gbc);
 
 			// Separation
 			this.minimalSpace(gbc, ++y);
@@ -81,12 +99,12 @@ public class GridInformation extends JPanel {
 			gbc.gridwidth = 2;
 			gbc.anchor = GridBagConstraints.WEST;
 			jlTmp = new JLabel("Model:");
-			this.add(jlTmp, gbc);
+			this.jIntPanel.add(jlTmp, gbc);
 			gbc.gridy = ++y;
 			gbc.gridx = 0;
 			gbc.gridwidth = 2;
 			jlTmp = new JLabel("  " + modelFeatures.getName(m));
-			this.add(jlTmp, gbc);
+			this.jIntPanel.add(jlTmp, gbc);
 
 			// Separation
 			this.minimalSpace(gbc, ++y);
@@ -97,13 +115,13 @@ public class GridInformation extends JPanel {
 			gbc.gridwidth = 2;
 			gbc.anchor = GridBagConstraints.WEST;
 			jlTmp = new JLabel("Perturbation:");
-			this.add(jlTmp, gbc);
+			this.jIntPanel.add(jlTmp, gbc);
 			gbc.gridy = ++y;
 			gbc.gridx = 0;
 			gbc.gridwidth = 2;
 			AbstractPerturbation ap = grid.getPerturbation(posX, posY);
 			jlTmp = new JLabel("  " + ((ap == null) ? "none" : ap.toString()));
-			this.add(jlTmp, gbc);
+			this.jIntPanel.add(jlTmp, gbc);
 
 			// Separation
 			this.minimalSpace(gbc, ++y);
@@ -113,7 +131,7 @@ public class GridInformation extends JPanel {
 			gbc.gridx = 0;
 			gbc.gridwidth = 1;
 			jlTmp = new JLabel("Proper:");
-			this.add(jlTmp, gbc);
+			this.jIntPanel.add(jlTmp, gbc);
 			gbc.gridwidth = 1;
 			for (String nodeID : lAllNodeIDs) {
 				if (this.compFeatures.getNodeInfo(nodeID).isInput())
@@ -122,13 +140,13 @@ public class GridInformation extends JPanel {
 				gbc.gridx = 0;
 				gbc.anchor = GridBagConstraints.WEST;
 				jlTmp = new JLabel(nodeID + " ");
-				this.add(jlTmp, gbc);
+				this.jIntPanel.add(jlTmp, gbc);
 				int index = grid.getNodeIndex(posX, posY, nodeID);
 				if (index < 0)
 					continue;
 				gbc.gridx = 1;
 				jlTmp = new JLabel(": " + grid.getCellState(posX, posY)[index]);
-				this.add(jlTmp, gbc);
+				this.jIntPanel.add(jlTmp, gbc);
 			}
 
 			// Separation
@@ -139,7 +157,7 @@ public class GridInformation extends JPanel {
 			gbc.gridx = 0;
 			gbc.gridwidth = 1;
 			jlTmp = new JLabel("Input:");
-			this.add(jlTmp, gbc);
+			this.jIntPanel.add(jlTmp, gbc);
 			gbc.gridwidth = 1;
 			for (String nodeID : lAllNodeIDs) {
 				if (!this.compFeatures.getNodeInfo(nodeID).isInput()
@@ -149,13 +167,13 @@ public class GridInformation extends JPanel {
 				gbc.gridx = 0;
 				gbc.anchor = GridBagConstraints.WEST;
 				jlTmp = new JLabel(nodeID + " ");
-				this.add(jlTmp, gbc);
+				this.jIntPanel.add(jlTmp, gbc);
 				int index = grid.getNodeIndex(posX, posY, nodeID);
 				if (index < 0)
 					continue;
 				gbc.gridx = 1;
 				jlTmp = new JLabel(": " + grid.getCellState(posX, posY)[index]);
-				this.add(jlTmp, gbc);
+				this.jIntPanel.add(jlTmp, gbc);
 			}
 
 			gbc.anchor = GridBagConstraints.PAGE_END;
@@ -169,11 +187,9 @@ public class GridInformation extends JPanel {
 		gbc.gridx = 0;
 		gbc.gridwidth = 2;
 		jlTmp = new JLabel(" ");
-		this.add(jlTmp, gbc);
+		this.jIntPanel.add(jlTmp, gbc);
 
 		// Repaint
-		// TODO: this.setSize(this.getPreferredSize());
-		this.revalidate();
-		this.repaint();
+		this.jIntPanel.revalidate();
 	}
 }
