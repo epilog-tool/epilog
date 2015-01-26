bfb4d629dc196f9c971c6624e1c8324ddd722634
PedroBLV <lisboa.ny@gmail.com>
2015-01-26 15:42:17 +0000

    Removed the Dimension line from the projDescPanel and now says Models Loaded instead

--- src/main/java/org/ginsim/epilog/gui/ProjDescPanel.java
+++ src/main/java/org/ginsim/epilog/gui/ProjDescPanel.java
@@ -16,7 +16,6 @@ public class ProjDescPanel extends JPanel {
 	private static final long serialVersionUID = -8691538114476162311L;
 
 	private static final String LABEL = "Dimensions: ";
-	private JLabel jlDimensions;
 	private JList<String> listSBMLs;
 
 	public ProjDescPanel() {
@@ -24,12 +23,6 @@ public class ProjDescPanel extends JPanel {
 		
 		// PAGE_START
 		JPanel jpWholeLine = new JPanel(new BorderLayout());
-		JLabel jlDim = new JLabel(LABEL);
-		Font font = jlDim.getFont();
-		jlDim.setFont(new Font(font.getFontName(), Font.BOLD, font.getSize()));
-		jpWholeLine.add(jlDim, BorderLayout.LINE_START);
-		this.jlDimensions = new JLabel();
-		jpWholeLine.add(this.jlDimensions, BorderLayout.CENTER);
 		this.add(jpWholeLine, BorderLayout.PAGE_START);
 
 		// CENTER
@@ -41,11 +34,6 @@ public class ProjDescPanel extends JPanel {
 		scroll.setMinimumSize(new Dimension(0, 100));
 		this.add(scroll, BorderLayout.CENTER);
 
-		this.clean();
-	}
-
-	public void setDimension(int w, int h) {
-		this.jlDimensions.setText(w + " x " + h);
 	}
 
 	public void addModel(String model) {
@@ -68,10 +56,6 @@ public class ProjDescPanel extends JPanel {
 		return this.listSBMLs.getModel().getSize();
 	}
 
-	public void clean() {
-		this.jlDimensions.setText("? x ?");
-		((DefaultListModel<String>) this.listSBMLs.getModel()).clear();
-	}
 
 	public String getSelected() {
 		return this.listSBMLs.getSelectedValue();
