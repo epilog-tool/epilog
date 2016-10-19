e7312b22803f0a82f8f4471a6e3cde13828fcce1
Pedro Varela <lisboa.ny@gmail.com>
2016-10-19 12:01:20 +0100

    Comments and adding options to SBML popupMenu

--- src/main/java/org/epilogtool/gui/menu/SBMLPopupMenu.java
+++ src/main/java/org/epilogtool/gui/menu/SBMLPopupMenu.java
@@ -9,12 +9,18 @@ import javax.swing.JPopupMenu;
 
 import org.epilogtool.gui.EpiGUI;
 
+/**
+ * Class that implements the options popup for the SBML. instead of going to the "SBML" menu bar the user can select the action here
+ *
+ */
 public class SBMLPopupMenu extends JPopupMenu {
 	private static final long serialVersionUID = 7968226829964184722L;
 
 	JMenuItem remove;
 
 	public SBMLPopupMenu() {
+		
+		// LOAD SBML
 		JMenuItem load = new JMenuItem("Load model (SBML)");
 		load.addActionListener(new ActionListener() {
 			@Override
@@ -29,6 +35,17 @@ public class SBMLPopupMenu extends JPopupMenu {
 		});
 		this.add(load);
 
+		// RENAME SBML
+		JMenuItem rename = new JMenuItem("Rename model (SBML)");
+		rename.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				EpiGUI.getInstance().renameSBML();
+			}
+		});
+		this.add(rename);
+		
+		// REMOVE SBML
 		this.remove = new JMenuItem("Remove model (SBML)");
 		remove.addActionListener(new ActionListener() {
 			@Override
@@ -37,6 +54,28 @@ public class SBMLPopupMenu extends JPopupMenu {
 			}
 		});
 		this.add(this.remove);
+		
+		
+		// Export SBML
+		JMenuItem export = new JMenuItem("Export model (SBML)");
+		export.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				EpiGUI.getInstance().exportSBML();
+			}
+		});
+		this.add(export);	
+		
+		// Replace SBML
+		JMenuItem replace = new JMenuItem("Replace model (SBML)");
+		replace.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				EpiGUI.getInstance().replaceSBML();
+			}
+		});
+		this.add(replace);	
+		
 	}
 
 	public void updateMenuItems(boolean enable) {
