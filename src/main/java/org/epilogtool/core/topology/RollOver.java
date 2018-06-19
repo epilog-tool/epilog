bab092794d185c4409ddd30a01e4ce1fd83d5f89
Pedro Varela <lisboa.ny@gmail.com>
2018-06-19 15:36:58 +0100

    BUG: vertical and horizontal rollover well identified

--- src/main/java/org/epilogtool/core/topology/RollOver.java
+++ src/main/java/org/epilogtool/core/topology/RollOver.java
@@ -31,17 +31,21 @@ public enum RollOver {
 
 	public static RollOver string2RollOver(String epiName, String str) {
 		// Note: The equals with additional options are for backward compatibility
-		if (str.equals(HORIZ.toString()) || str.equals("Horizontal"))
+		if (str.equals(HORIZ.toString()) || str.equals("Horizontal")||  str.equals("Cylinder_(horizontal_wrap)"))
 			return HORIZ;
-		else if (str.equals(VERT.toString()) || str.equals("Vertical"))
+		else if (str.equals(VERT.toString()) || str.equals("Vertical") ||  str.equals("Cylinder_(vertical_wrap)"))
 			return VERT;
-		else if (str.equals(NONE.toString()) || str.equals("NoRollover")||str.equals("Rectangular"))
+		else if (str.equals(NONE.toString()) || str.equals("NoRollover")||str.equals("Rectangular")||  str.equals("Rectangular_(no_wrap)"))
 			return NONE;
-		else if (str.equals(HORIZ_VERT.toString()) || str.equals("Torus") || str.equals("Horizontal&Vertical")||str.equals("Torus"))
+		else if (str.equals(HORIZ_VERT.toString()) || str.equals("Torus") || str.equals("Horizontal&Vertical")||str.equals("Torus_(both_wraps)"))
 			return HORIZ_VERT;
 		else {
 			JOptionPane.showMessageDialog(null,
 					epiName + ": Loaded border option incorrect. Border set to rectangular.");
+			System.out.println("WE "+str.equals(HORIZ.toString()));
+			System.out.println("WE "+str.equals(VERT.toString()));
+			System.out.println(str);
+			
 			return NONE;
 		}
 	}
